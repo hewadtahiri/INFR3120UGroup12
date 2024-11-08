bcccbc6f19e18a69784de9bdc9bb142a159c054b
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-08 01:26:16 -0500

    Updated Home.ejs.

--- Controllers/index.js
+++ Controllers/index.js
@@ -5,16 +5,4 @@ router.get("/", (req, res) => {
   res.render("Layout", { title: "Home", body: "Home" });
 });
 
-router.get("/Cars", (req, res) => {
-  res.render("Layout", { title: "Cars", body: "Cars" });
-});
-
-router.get("/Reservations", (req, res) => {
-  res.render("Layout", { title: "Reservations", body: "Reservations" });
-});
-
-router.get("/Login", (req, res) => {
-  res.render("Layout", { title: "Login", body: "Login" });
-});
-
 module.exports = router;
