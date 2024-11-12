6f55b4e5cbb48c9c8af088ff2c1916ba3dcd8532
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-12 01:55:47 -0500

    Updated landing page.

--- Routes/index.js
+++ Routes/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-// let mongoose = require('mongoose');
 let reservations = [];
-// let Reservation = require('../Models/reservation');
+// let mongoose = require("mongoose");
+// let reservation = require("../Models/reservation");
 
 router.get("/", (req, res) => {
   res.render("Layout", { title: "Home", body: "Home", reservations });
 });
 
-/*Code for Edit operation
-router.get('/edit/:id',async(req,res,next)=>{
+/* Code for Edit operation
+router.get("/edit/:id",async(req,res,next)=>{
   try{
       const id = req.params.id;
-      const reservationToEdit = await Reservation.findById(id);
-      res.render('Reservation/edit',
+      const reservationToEdit = await Resereservationrvation.findById(id);
+      res.render("reservation/edit",
           {
-              title: 'Edit Reservation',
-              Reservation:reservationToEdit
+              title: "Edit Reservation",
+              reservation:reservationToEdit
           }
       )
   }
@@ -28,44 +28,44 @@ router.get('/edit/:id',async(req,res,next)=>{
   }
 });
 
-router.post('/edit/:id',async(req,res,next)=>{
+router.post("/edit/:id",async(req,res,next)=>{
   try{
       let id= req.params.id;
-      let updatedReservation = Reservation({
+      let updatedReservation = reservation({
           "_id":id,
           "CustomerName:":req.body.CustomerName,
           "CarModel:":req.body.CarModel,
           "ReservationDate:":req.body.ReservationDate
 
       })
-      Reservation.findByIdAndUpdate(id, updatedReservation).then(()=>{
-          res.redirect('/')
+      reservation.findByIdAndUpdate(id, updatedReservation).then(()=>{
+          res.redirect("/")
       })
   }
   catch(err)
   {
       console.error(err);
-      res.render('Reservation/list',{
-          error: 'Error on the server'
+      res.render("reservation/list",{
+          error: "Error on the server."
       })
   }
-});*/
+}); */
 
-/*Code for Delete operation
-router.get('/delete/:id',async(req,res,next)=>{
+/* Code for Delete operation
+router.get("/delete/:id",async(req,res,next)=>{
   try{
       let id=req.params.id;
-      Reservation.deleteOne({_id:id}).then(()=>{
-          res.redirect('/')
+      reservation.deleteOne({_id:id}).then(()=>{
+          res.redirect("/")
       })
   }
   catch(err)
   {
       console.error(err);
-      res.render('Reservation/list',{
-          error: 'Error on the server'
+      res.render("reservation/list",{
+          error: "Error on the server."
       })
   }
-});*/
+}); */
 
 module.exports = router;
