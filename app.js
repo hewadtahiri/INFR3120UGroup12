ae780a8eba2761ddb24735a4586b6c284f709c99
Desmond During <desmond.during@ontariotechu.net>
2024-11-26 10:32:39 -0500

    Installed Libraries for Authentication.

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const routes = require("./Routes/index");
 const app = express();
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let passportGoogle = require('passport-google-oauth');
+let localStrategy = passportLocal.Strategy;
+let flash = require(connect-flash);
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Views"));
