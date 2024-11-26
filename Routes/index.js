94cfedf4317891d27c8bac4f4002de1f0a9eaced
Desmond During <desmond.during@ontariotechu.net>
2024-11-26 13:36:26 -0500

    Added Authentication.

--- Routes/index.js
+++ Routes/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
+let userModel = require('../Models/user');
+let User = userModel.User;
 
 // Simulates the database using an array.
 let reservations = [];
+
+// Authentication Verification.
+function authVerification(req, res, next)
+{
+  if(!req.isAuthenticated())
+    {
+      return res.redirect('/login')
+    }
+    next();
+}
 
 // Displays home page and active reservations.
 router.get("/", (req, res) => {
@@ -10,7 +23,7 @@ router.get("/", (req, res) => {
 });
 
 // Creates a new reservation.
-router.post("/reservations", (req, res) => {
+router.post("/reservations", authVerification, (req, res) => {
   const newReservation = {
     id: Date.now(),
     customer_name: req.body.customer_name,
@@ -22,7 +35,7 @@ router.post("/reservations", (req, res) => {
 });
 
 // Edits an existing reservation.
-router.get("/reservations/edit/:id", (req, res) => {
+router.get("/reservations/edit/:id", authVerification, (req, res) => {
   const reservationId = req.params.id;
   const reservation = reservations.find(r => r.id == reservationId);
 
@@ -39,7 +52,7 @@ router.get("/reservations/edit/:id", (req, res) => {
 });
 
 // Updates an existing reservation.
-router.post("/reservations/edit/:id", (req, res) => {
+router.post("/reservations/edit/:id", authVerification, (req, res) => {
   const reservation = reservations.find(r => r.id == req.params.id);
   if (reservation) {
     reservation.customer_name = req.body.customer_name;
@@ -50,9 +63,119 @@ router.post("/reservations/edit/:id", (req, res) => {
 });
 
 // Deletes an existing reservation.
-router.get("/reservations/delete/:id", (req, res) => {
+router.get("/reservations/delete/:id", authVerification, (req, res) => {
   reservations = reservations.filter(r => r.id != req.params.id);
   res.redirect("/#reservations");
 });
 
+// Displays Login page.
+router.get("/login", (req, res) => {
+  if (!req.user)
+  {
+    res.render("Authentication/login", { 
+      title: "Login", 
+      body: "Login",
+      displayName: req.user ? req.user.displayName: '', 
+      reservations,
+      editReservation: null });
+  }
+
+  else
+  {
+    return res.redirect('/');
+  }
+
+});
+
+// Posts Login page.
+router.post("/login", (req, res, next) => {
+  passport.authenticate('local',(err,user)=>
+    {
+      // Error Verification
+
+      // Server
+      if(err)
+        {
+          return next(err);
+        }
+      
+      // Login
+      if(!user)
+        {
+          req.flash('AuthenticationError');
+          return res.redirect('/login');
+        }
+      req.login(user,(err) => {
+        if(err)
+        {
+          return next(err)
+        }
+        return res.redirect('/#reservations')
+      })
+      
+    })(req,res,next)
+});
+
+// Displays Registration page.
+router.get("/register", (req, res) => {
+  if (!req.user)
+  {
+    res.render("Authentication/register", { 
+      title: "Register", 
+      body: "Register",
+      displayName: req.user ? req.user.displayName: '', 
+      reservations,
+      editReservation: null });
+  }
+
+  else
+  {
+    return res.redirect('/');
+  }
+
+});
+
+// Posts Login page.
+router.post("/register", (req, res) => {
+  let registeredUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    displayName: req.body.displayName,
+  })
+  User.register(registeredUser, req.body.password, (err) => {
+    if(err)
+    {
+      console.log("Error: Cannot Create new user");
+    
+
+      if(err.name=="ExistingUserError")
+      {
+        req.flash('registerMessage',
+        'Registration Error: User Already Exists.');
+          
+      }
+
+      return res.render('Authentication/register',
+      {
+        title: "Register", 
+        body: "Register",
+        displayName: req.user ? req.user.displayName: '', 
+        reservations,
+        editReservation: null
+      });
+    }
+    else
+    {
+      return passport.authenticate('local')(req,res,()=>{
+        res.redirect('/#reservations')
+      })
+    }
+  })
+});
+
+// User Logout.
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const path = require("path");
 const routes = require("./Routes/index");
 const app = express();
 
+// User Model Creation
+let userModel = require('./Models/user.js');
+let User = userModel.User;
+
 // Creating Authentication Dependencies
 let session = require('express-session');
 let passport = require('passport');
 let passportLocal = require('passport-local');
 let passportGoogle = require('passport-google-oauth');
+passport.use(User.createStrategy());
 let localStrategy = passportLocal.Strategy;
-let flash = require(connect-flash);
+let flash = require('connect-flash');
 
 // Setting up Cookies
 app.use(session({
@@ -20,16 +25,14 @@ app.use(session({
 
 // Initializing Dependencies
 app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// User Model Creation
-let userModel = require('../Models/user');
-let user = userModel.User;
 
 // Encrypting and Decrypting user data
-passport.serializeUser(user.serializeUser());
-passport.deserializeUser(user.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Views"));
