842964357b229931de7fd9ae885db7b405ab0e77
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-28 14:41:22 -0500

    Updated index.js.

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
-let User = require("../Models/User").schema;
+const User = require("../Models/User");
 
 // Simulates the database using an array.
 let reservations = [];
